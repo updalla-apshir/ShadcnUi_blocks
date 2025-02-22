da9d964775c21301792dcec506445dc1709ee5c3
Meschac Irung <meschacirung@gmail.com>
2025-02-22 20:15:47 +0200

    feat: update BlockPreview component to include category prop for enhanced categorization

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,10 @@
 import BlockPreview from '@/components/BlockPreview'
 
-const code = `"use client"
- 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
- 
-import { Button } from "@/components/ui/button"
-
-export const ThemeToggle = () => {
-    const { theme, setTheme } = useTheme()
-     
-    return (
-        <Button
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-            variant="ghost"
-            className="size-8"
-        >
-            {theme === "dark" ? <Sun /> : <Moon />}
-        </Button>
-    )
-}`
-
 export default function Home() {
     return (
         <>
-            <BlockPreview preview="https://html.tailus.io/blocks-preview/pricing/one" code={code} title="title 1" />
-            <BlockPreview preview="https://html.tailus.io/blocks-preview/team/one" code="<div/>" title="title 2" />
+            <BlockPreview preview="https://html.tailus.io/blocks-preview/pricing/one" code={`<div />`} category="pricing" title="title 1" />
+            <BlockPreview preview="https://html.tailus.io/blocks-preview/team/one" code="<div/>" category="pricing" title="title 2" />
         </>
     )
 }
