d5058117e20f6adeb852ff03062a5a48bdf7d172
Meschac Irung <meschacirung@gmail.com>
2025-03-04 10:00:25 +0200

    fix: remove trailing slash from base URL in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import { blocks, categories } from '@/data/blocks'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://nsui.irung.me/' 
+  const baseUrl = 'https://nsui.irung.me' 
   
   const routes = [
     {
