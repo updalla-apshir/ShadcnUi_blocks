26f24cf7615bfeaf1080b9cb1957bced4f979a27
Meschac Irung <meschacirung@gmail.com>
2025-03-04 09:51:27 +0200

    fix: update sitemap URL in robots.txt for correct routing

--- app/robot.ts
+++ app/robot.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/'], 
     },
-    sitemap: 'https://your-domain.com/sitemap.xml',
+    sitemap: 'https://nsui.irung.me/sitemap.xml',
   }
 }
